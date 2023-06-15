71970b7c5a7caf02fcbf6895889caba3b35588c6
chenlong0313 <chenlong0313@digiwin.com>
2023-06-15 23:39:20 +0800

    feat: 测试元数据被修改->transformData

--- src/app/common-modules/info/info.component.ts
+++ src/app/common-modules/info/info.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { produce } from 'immer';
 
 @Component({
   selector: 'common-info',
@@ -8,13 +9,40 @@ export class InfoComponent implements OnInit {
   @Input() data: any;
   @Output() callbackEmit: EventEmitter<any> = new EventEmitter();
 
+  _originData: any = {
+    info: {
+      name: 'Jack',
+    },
+  };
+
+  transformData: any = {};
+
+  set originData(value: any) {
+    this._originData = value;
+    this.transformDataFn(value);
+  }
+
+  get originData() {
+    return this._originData;
+  }
+
   date = null;
 
   constructor() {}
 
   ngOnInit() {}
 
+  transformDataFn(value: any) {
+    console.log('transform-->', value.info.name);
+    this.transformData = value;
+  }
+
   callback() {
     this.callbackEmit.emit('emit success!');
+    for (let index = 0; index < 1000; index++) {
+      this.originData = produce(this.originData, (draft: any) => {
+        draft.info.name = { age: 'Tom' + index };
+      });
+    }
   }
 }
